b8fa045f40e0db7256587b60144bf68992ad5b80
vikram087 <48704830+vikram087@users.noreply.github.com>
2020-04-30 20:00:43 -0700

    Update ColorWheel.java

--- ColorWheel.java
+++ ColorWheel.java
@@ -8,15 +8,14 @@ public class DrawCircle extends JPanel
                             implements ActionListener{
 	
 	// Draws the color wheel. 
-	 public void paintComponent(Graphics g)
-	  {
-		 
-		Graphics2D g2 = (Graphics2D) g.create(); 
-		 
+	public void paintComponent(Graphics g)
+	{
+		
+		Graphics2D g2 = (Graphics2D) g.create();  
 		int centerX = this.getWidth()/2;
 		int centerY = this.getHeight()/2;
-		float[] fractions = new float[] {(float)1/5, (float)2/7, (float)3/7, (float)4/7, (float)5/7, (float)6/7, (float)1};
-		Color[] colors = new Color[] {Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA, Color.RED, Color.ORANGE, Color.YELLOW};
+		float[] fractions = new float[] {(float)1/5, (float)2/8, (float)3/8, (float)4/8, (float)5/8, (float)6/8, (float)7/8, (float)1};
+		Color[] colors = new Color[] {Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA, Color.PINK, Color.RED, Color.ORANGE, Color.YELLOW};
 		
 		
 		RadialGradientPaint gradient = new RadialGradientPaint((float)(centerX - 100), (float)(centerY - 100), 250, fractions, colors);
@@ -32,21 +31,21 @@ public class DrawCircle extends JPanel
 	    //g.setColor(Color.BLUE);
 	    g2.fillArc(centerX - 100, centerY - 100, 200, 200, 240, 120);
 	    
-	  }
+	}
 	
-	 // This might be unnecessary, depends on if we want a button or not.
-	 public void actionPerformed(ActionEvent e) {
+	// This might be unnecessary, depends on if we want a button or not.
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 	
 	}
 	
-	 public static void main(String[] args)
-	  {
+	public static void main(String[] args)
+	{
 	    JFrame frame = new JFrame("ColorWheel");
 	    frame.setBounds(600,300,700,700);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    Container c = frame.getContentPane();
 	    c.add(new DrawCircle());
 	    frame.setVisible(true);
-	  }
+	 }
 }
