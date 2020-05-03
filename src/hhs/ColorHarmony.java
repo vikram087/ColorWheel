365c568108833c70acfddb1bc98d07f4f134f16d
Rainman093 <61221858+Rainman093@users.noreply.github.com>
2020-05-03 11:06:23 -0700

    Added 4 Classes, ColorHarmony is Interface

---
+++ src/hhs/ColorHarmony.java
@@ -0,0 +1,24 @@
+/** 
+Author: Vikram Penumarti
+Date: 4/30/2020
+Rev: 01
+Notes:
+*/
+
+/**
+ * Author: Richard Zhang
+ * Date: 4/30/2020
+ * Rev:01
+ * Notes: 
+ */
+package hhs;
+
+
+import java.awt.*;
+
+public interface ColorHarmony {
+	public Color color();
+	
+	public void getColor();
+	public void findHarmony();
+}
---
+++ src/hhs/MonochromaticHarmony.java
@@ -0,0 +1,35 @@
+/** 
+Author: Vikram Penumarti
+Date: 4/30/2020
+Rev: 01
+Notes:
+*/
+
+/**
+ * Author: Richard Zhang
+ * Date: 4/30/2020
+ * Rev:01
+ * Notes: 
+ */
+package hhs;
+
+import java.awt.Color;
+
+public class MonochromaticHarmony implements ColorHarmony{
+
+	// Override
+	public Color color() {
+		return null;
+	}
+
+	// Override
+	public void getColor() {
+		
+	}
+
+	// Override
+	public void findHarmony() {
+		
+	}
+
+}
