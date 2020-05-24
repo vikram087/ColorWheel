f85748d171d654ec73312443d3da517054d6f93b
Rainman093 <61221858+Rainman093@users.noreply.github.com>
2020-05-23 18:36:34 -0700

    Renamed Circle, Added the Extends

--- src/Circle.java
+++ src/Circle.java
@@ -15,22 +15,18 @@
 import java.awt.*;
 import java.awt.geom.Arc2D;
 
-/**
- * REV 2, 5/5/2020
- * KNOWN BUGS: GRADIENT ACHIEVED FOR EACH INDIVIDUAL COLOR
- * BUT DOES NOT HAVE SMOOTH TRANSITIONS
- */
 
-public class DrawCircle  
+
+public class Circle extends Polygon
 {
 	
-	public void draw(int centerX, int centerY, Graphics2D g2)
+	public void draw(int xCenter, int yCenter, Graphics2D g2)
 	{
 		
-		double sizeX = centerX * 2 / 3;// 4/3 ratio from frame to arc
-		double sizeY = centerY * 2 / 3;
+		double sizeX = xCenter * 2 / 3;// 4/3 ratio from frame to arc
+		double sizeY = yCenter * 2 / 3;
 		
-		//System.out.println(centerX + "," + centerY);
+		//System.out.println(xCenter + "," + yCenter);
 		
 		double angle = 0;
 		double addedAngle = 0.2353;
@@ -40,37 +36,37 @@ public class DrawCircle
 		for (int rgb = 0; rgb <= maxWhite; rgb++)
 		{
 			g2.setPaint(new Color(255, rgb, rgb));//red
-			g2.fill(new Arc2D.Double(centerX - sizeX / 2, centerY - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
+			g2.fill(new Arc2D.Double(xCenter - sizeX / 2, yCenter - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
 			angle = angle + addedAngle;
 		}
 		for (int rgb = 0; rgb <= maxWhite; rgb++)
 		{
 			g2.setColor(new Color(255, 255, rgb));//purple
-			g2.fill(new Arc2D.Double(centerX - sizeX / 2, centerY - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
+			g2.fill(new Arc2D.Double(xCenter - sizeX / 2, yCenter - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
 			angle = angle + addedAngle;
 		}
 		for (int rgb = 0; rgb <= maxWhite; rgb++)
 		{
 			g2.setColor(new Color(rgb, 255, rgb));//blue
-			g2.fill(new Arc2D.Double(centerX - sizeX / 2, centerY - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
+			g2.fill(new Arc2D.Double(xCenter - sizeX / 2, yCenter - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
 			angle = angle + addedAngle;
 		}
 		for (int rgb = 0; rgb <= maxWhite; rgb++) 
 		{
 			g2.setColor(new Color(rgb, 255, 255));//cyan
-			g2.fill(new Arc2D.Double(centerX - sizeX / 2, centerY - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
+			g2.fill(new Arc2D.Double(xCenter - sizeX / 2, yCenter - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
 			angle = angle + addedAngle;
 		}
 		for (int rgb = 0; rgb <= maxWhite; rgb++) 
 		{
 			g2.setColor(new Color(rgb, rgb, 255));//green
-			g2.fill(new Arc2D.Double(centerX - sizeX / 2, centerY - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
+			g2.fill(new Arc2D.Double(xCenter - sizeX / 2, yCenter - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
 			angle = angle + addedAngle;
 		}
 		for (int rgb = 0; rgb <= maxWhite; rgb++)
 		{
 			g2.setColor(new Color(255, rgb, 255));//yellow
-			g2.fill(new Arc2D.Double(centerX - sizeX / 2, centerY - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
+			g2.fill(new Arc2D.Double(xCenter - sizeX / 2, yCenter - sizeY / 2, sizeX, sizeY, angle, addedAngle, Arc2D.PIE));
 			angle = angle + addedAngle;
 		}
 	}
@@ -82,21 +78,21 @@ public class DrawCircle
 //				(float) 6 / 8, (float) 7 / 8, (float) 1 };
 //		Color[] colors = new Color[] { Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA, Color.PINK, Color.RED,
 //				Color.ORANGE, Color.YELLOW };
-//		RadialGradientPaint gradient = new RadialGradientPaint((float) (centerX - 100), (float) (centerY - 100), 250,
+//		RadialGradientPaint gradient = new RadialGradientPaint((float) (xCenter - 100), (float) (yCenter - 100), 250,
 //				fractions, colors);
 //
 //		// test
 //		float[] fractionTest1 = new float[] { (float) 1 / 5, (float) 2 / 8 };
 //		Color[] colorsTest1 = new Color[] { Color.GREEN, Color.CYAN };
-//		RadialGradientPaint gradientTest1 = new RadialGradientPaint((float) (centerX - 100), (float) (centerY - 100),
+//		RadialGradientPaint gradientTest1 = new RadialGradientPaint((float) (xCenter - 100), (float) (yCenter - 100),
 //				250, fractionTest1, colorsTest1);
 //
 //		super.paintComponent(g);
 //		g2.setPaint(gradient);
 //		g2.setPaint(gradientTest1);
-//	    g2.fillArc(centerX - 100, centerY - 100, 200, 200, 0, 120);
-//	    g2.fillArc(centerX - 100, centerY - 100, 200, 200, 120, 120);
-//	    g2.fillArc(centerX - 100, centerY - 100, 200, 200, 240, 120);	    
+//	    g2.fillArc(xCenter - 100, yCenter - 100, 200, 200, 0, 120);
+//	    g2.fillArc(xCenter - 100, yCenter - 100, 200, 200, 120, 120);
+//	    g2.fillArc(xCenter - 100, yCenter - 100, 200, 200, 240, 120);	    
 
 	
 	// Basic Color Wheel
@@ -114,21 +110,21 @@ public class DrawCircle
 //				if (x == 0) 
 //				{
 //					g.setColor(Color.red);
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle);
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle);
 //				}
 	//
 //				// ORANGE
 //				if (x == 1)
 //				{
 //					g.setColor(Color.orange);
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle);
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle);
 //				}
 	//
 //				// YELLOW
 //				if (x == 2) 
 //				{
 //					g.setColor(Color.yellow);
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle);
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle);
 //				}
 	//
 //				// GREEN 1(NEED TO FIGURE OUT SHADES
@@ -136,7 +132,7 @@ public class DrawCircle
 //				{
 //					g.setColor(Color.green);
 	//
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle );
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle );
 //				}
 	//
 //				// GREEN 2(TOP)
@@ -144,7 +140,7 @@ public class DrawCircle
 //				{
 //					g.setColor(Color.green);
 	//
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle);
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle);
 //				}
 	//
 //				// GREEN 3(SAME)
@@ -152,14 +148,14 @@ public class DrawCircle
 //				{
 //					g.setColor(Color.green);
 	//
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle);
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle);
 //				}
 	//
 //				// CYAN
 //				if (x == 6) 
 //				{
 //					g.setColor(Color.CYAN);
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle );
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle );
 //				}
 	//
 //				// BLUE 1(SUPPOSED TO BE MIX OF BLUE AND CYAN
@@ -168,28 +164,28 @@ public class DrawCircle
 //					g.setColor(Color.blue);
 	//
 //					// SAME thing as Green, Figure out how to get different shades.
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle);
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle);
 //				}
 	//
 //				// PURPLE OR MAGENTA
 //				if (x == 8) 
 //				{
 //					g.setColor(Color.MAGENTA);
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle);
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle);
 //				}
 	//
 //				// AGAIN, PURPLE OR MAGENTA
 //				if (x == 9) 
 //				{
 //					g.setColor(Color.MAGENTA);
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle );
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle );
 //				}
 	//
 //				// PINK
 //				if (x == 10) 
 //				{
 //					g.setColor(Color.PINK);
-//					g.fillArc(centerX - 100, centerY - 100, 200, 200, angle, fixedAngle );
+//					g.fillArc(xCenter - 100, yCenter - 100, 200, 200, angle, fixedAngle );
 //				}
 	//
 //				angle = angle + 36;
@@ -205,7 +201,7 @@ public class DrawCircle
 //			for(int blue = 0; blue <= 255; blue++)
 //			{
 //				g2.setColor(new Color(red, green, blue));
-//				g2.fill(new Arc2D.Double(centerX - 100, centerY - 100, 200, 200, angle, 1/(Math.pow(2, 24)), Arc2D.PIE));
+//				g2.fill(new Arc2D.Double(xCenter - 100, yCenter - 100, 200, 200, angle, 1/(Math.pow(2, 24)), Arc2D.PIE));
 //				angle = angle + 1/(Math.pow(2, 24));
 //			}
 //		}
